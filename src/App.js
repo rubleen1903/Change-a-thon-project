11e3871003711a2a07b8dee1334314e04db2a433
rubleen1903 <rubleen16@gmail.com>
2021-05-09 04:53:29 +0530

    done with the home-page

--- src/App.js
+++ src/App.js
@@ -44,16 +44,35 @@ class App extends React.Component {
       <Router>
       <Header/>
       
-      <Link to = "/" >
-      <img className = "home-img"
-      src = { logo }
-      alt = "Greenergy" / >
-      </Link> 
+      
       <Container>
-        <Row>
-          <Col className = "Form">
+        <Row style={{ marginTop: 0, marginBottom: 50 }}>
+        <Col lg={2} md={4} xs={6}>
+          <div className="heading">
+          <h1>
+            Inspiration
+          </h1>
+          </div>
+          </Col>
+
+          <Col lg={8} md={4} xs={6}>
+          <div className="home-img">
+          <Link to = "/" >
+      <img className="home-img"
+      src = { logo }
+      alt = "Greenergy"/>
+      </Link> 
+      </div>
+          </Col>
+          <Col lg={1} md={2} xs={6}>
+            <p className="content">
+            Global Warming, carbon emissions are current buzzwords. Computer science is changing the way we look at climate change and perhaps even plays a role in slowing that phenomenon.  
+But there is a downside to the excessive use of technology as well. Computer doesn't necessarily run on carbon fuels however the electricity it uses to run daily does and the amount used has been piling up over the past few decades now as more people buy home PC units to enjoy the world of computing
+
+            </p>
        
           </Col>
+        
         </Row>
       </Container>
       <Route path ="\calcenergy" component ={calcenergy}></Route>
