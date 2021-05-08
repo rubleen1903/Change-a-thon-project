23d9f70ef6d35aa8f6bef159c5bad715fb3431c5
rubleen1903 <rubleen16@gmail.com>
2021-05-09 04:20:30 +0530

    started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import { Switch,Route, BrowserRouter as Router, Link } from 'react-router-dom';
+import React from "react";
+import calcenergy from './components/calcenergy';
+import { Row, Col, Container } from 'react-bootstrap';
+
+
+import './App.css';
+import logo from './assets/logo.png';
+
+
+const Header = () => {
+  return ( <div className = "main-header" >
+      <nav className = "header-nav" >
+      <Link to = "/" >
+      <img className = "logo"
+      src = { logo }
+      alt = "Greenergy" />
+      </Link> 
+      <div className = "header-links" >
+      <Link className = "subtitle header-text"
+      to = "/calcenergy" >
+      Calculate Energy </Link> 
+      <Link className = "subtitle header-text"
+      to = "/energysugg" >
+      Energy Suggestions </Link> 
+       </div> </nav> </div>
   );
+};
+
+const Footer = () => {
+  return ( <div className = "main-footer" >
+      <nav className = "footer-nav" >
+      <a className = "subtitle" href = "/" >Github </a> 
+      <a className = "subtitle" href = "/" >About </a> </nav> </div>
+  );
+};
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+      <Header/>
+      
+      <Link to = "/" >
+      <img className = "home-img"
+      src = { logo }
+      alt = "Greenergy" / >
+      </Link> 
+      <Container>
+        <Row>
+          <Col className = "Form">
+       
+          </Col>
+        </Row>
+      </Container>
+      <Route path ="\calcenergy" component ={calcenergy}></Route>
+      <Footer/>
+      </Router>
+    ) 
+  }
 }
 
 export default App;
---
+++ src/components/calcenergy.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Row, Col, Container } from 'react-bootstrap';
+
+import './calcenergy.css';
+
+const calcenergy = () =>
+{ return (
+    <Container fluid style={{ backgroundColor: '#FFE3FE' }}>
+    <Container
+      fluid="md"
+      style={{ backgroundColor: 'white' }}
+    >
+    <p className="heading">
+    Calculate your Energy Here 
+</p>
+</Container>
+</Container>
+
+  );
+};
+
+export default calcenergy;
